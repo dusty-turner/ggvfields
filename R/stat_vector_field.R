23a707880a325af6c8ce255a334b1e898a5c6b5e
Turner <dusty.s.turner@gmail.com>
2024-06-26 15:31:51 -0500

    transitioned code to go from working with data to working with x and y limits

--- R/stat_vector_field.R
+++ R/stat_vector_field.R
@@ -17,44 +17,71 @@
 #' @import ggplot2
 #'
 #' @examples
+
+ensure_nonempty_data <- function(data) {
+  if (is_empty(data)) {
+    data.frame(x = 1)
+  }
+  else {
+    data
+  }
+}
+
 stat_vector_field <- function(mapping = NULL, data = NULL, geom = "segment",
-                              position = "identity", ..., fun = NULL,
-                              scaling_factor = 1, na.rm = FALSE, show.legend = NA,
-                              inherit.aes = TRUE) {
+                              position = "identity", na.rm = FALSE,
+                              show.legend = NA, inherit.aes = TRUE,
+                              fun, xlim, ylim, n = 10, ...) {
+
+  if (is.null(data)) data <- ensure_nonempty_data
+
   layer(
     stat = StatVectorField,
-    mapping = mapping,
     data = data,
+    mapping = mapping,
     geom = geom,
     position = position,
     show.legend = show.legend,
     inherit.aes = inherit.aes,
-    params = list(na.rm = na.rm, fun = fun, scaling_factor = scaling_factor, ...)
+    params = list(
+      fun = fun,
+      xlim = xlim,
+      ylim = ylim,
+      n = n,
+      na.rm = na.rm,
+      ...
+    )
   )
 }
 
 
-StatVectorField <-
-  ggproto("StatVectorField", Stat,
-          required_aes = c("x", "y"),
 
-          compute_group = function(data, scales, fun = NULL, scaling_factor = 1,...) {
-            if (is.null(fun)) {
-              stop("A transformation function must be provided")
-            }
+StatVectorField <- ggproto("StatVectorField", Stat,
 
-            transformed <- fun(data$x, data$y)
+                           # setup_data = cpt_data,
 
-            data <-
-              data.frame(x = data$x,
-                         y = data$y,
-                         u = transformed[[1]],
-                         v = transformed[[2]]
-              ) |>
-              mutate(magnitude = sqrt(u^2 + v^2)) |>
-              mutate(u_norm = u / magnitude, v_norm = v / magnitude) |>
-              mutate(xend = x + u_norm * scaling_factor, yend = y + v_norm * scaling_factor)
+                           # required_aes = c("x","y"),
 
-            return(data)
-          }
-  )
+                           compute_group = function(data, scales, fun, xlim, ylim, n = n) {
+                             # Create a sequence of x and y values within the limits
+                             x_seq <- seq(xlim[1], xlim[2], length.out = n)
+                             y_seq <- seq(ylim[1], ylim[2], length.out = n)
+                             grid <- expand.grid(x = x_seq, y = y_seq)
+
+                             # Evaluate the function to get vector components
+                             vectors <- fun(grid$x, grid$y)
+
+                             # Create a data frame for geom_segment
+                             data <-
+                               data.frame(x = grid$x,
+                                          y = grid$y,
+                                          u = vectors[[1]],
+                                          v = vectors[[2]]
+                               ) |>
+                               mutate(magnitude = sqrt(u^2 + v^2)) |>
+                               mutate(u_norm = u / magnitude, v_norm = v / magnitude) |>
+                               mutate(xend = x + u_norm, yend = y + v_norm)
+
+                             data
+                           }
+
+)
