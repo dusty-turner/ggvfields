f93a782cc3d551e7a6fc09e49a8859c201531324
dusty-turner <dusty.s.turner@gmail.com>
2024-07-15 12:25:54 -0500

    computed variables in documentation

--- R/vector_field.R
+++ R/vector_field.R
@@ -57,6 +57,15 @@
 #'     arrow = arrow(length = unit(1, "mm"))
 #'   )
 #'
+#' @section Computed variables:
+#'
+#'   \describe{
+#'     \item{norm}{The magnitude of the vector field, computed as \eqn{\|\mathbf{w}\| = \sqrt{u^2 + v^2}}.}
+#'     \item{divergence}{The divergence of the vector field, computed as \eqn{\nabla \cdot \mathbf{F} = \frac{\partial f_x}{\partial x} + \frac{\partial f_y}{\partial y}}.}
+#'     \item{curl}{The curl of the vector field, computed as \eqn{\text{curl} \, \mathbf{F} = \frac{\partial f_y}{\partial x} - \frac{\partial f_x}{\partial y}}.}
+#'     \item{laplacian}{The Laplacian of the vector field, computed as \eqn{\Delta \mathbf{F} = \frac{\partial^2 f_x}{\partial x^2} + \frac{\partial^2 f_x}{\partial y^2} + \frac{\partial^2 f_y}{\partial x^2} + \frac{\partial^2 f_y}{\partial y^2}}.}
+#'     \item{directional_derivative}{The directional derivative of the vector field, computed as \eqn{D_{\mathbf{v}} \mathbf{F} = \frac{\partial F_x}{\partial x} v_x + \frac{\partial F_x}{\partial y} v_y + \frac{\partial F_y}{\partial x} v_x + \frac{\partial F_y}{\partial y} v_y}.}
+#'   }
 NULL
 
 
