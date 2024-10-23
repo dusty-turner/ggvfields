c1f3d227f7a4821eca43fdd00c89ddbc9bbf7a86
Turner <dusty.s.turner@gmail.com>
2024-10-23 14:27:52 -0500

    arrow heads fill and color now only react to color

--- R/geom_vector.R
+++ R/geom_vector.R
@@ -221,7 +221,7 @@ draw_panel_vector <- function(
     vector_grob <- grid::segmentsGrob(
       x0 = unit(coords$x, "npc"), y0 = unit(coords$y, "npc"),
       x1 = unit(coords$xend, "npc"), y1 = unit(coords$yend, "npc"),
-      gp = grid::gpar(col = coords$colour, fill = coords$fill, lwd = linewidth),
+      gp = grid::gpar(col = coords$colour, fill = coords$colour, lwd = linewidth),
       arrow = arrow
     )
 
@@ -299,7 +299,7 @@ draw_panel_vector <- function(
         y0 = unit(coords$y, "npc") - unit(half_dy, "cm"),
         x1 = unit(coords$x, "npc") + unit(half_dx, "cm"),
         y1 = unit(coords$y, "npc") + unit(half_dy, "cm"),
-        gp = grid::gpar(col = coords$colour, fill = coords$fill, lwd = linewidth),  # Use mapped fill
+        gp = grid::gpar(col = coords$colour, fill = coords$colour, lwd = linewidth),  # Use mapped fill
         arrow = arrow  # Pass the arrow parameter here
       )
       points_grob <- NULL
@@ -322,7 +322,7 @@ draw_panel_vector <- function(
         x0 = unit(coords$x, "npc"), y0 = unit(coords$y, "npc"),
         x1 = unit(coords$x, "npc") + unit(coords$length * coords$dx, "cm"),
         y1 = unit(coords$y, "npc") + unit(coords$length * coords$dy, "cm"),
-        gp = grid::gpar(col = coords$colour, fill = coords$fill, lwd = linewidth),  # Use mapped fill
+        gp = grid::gpar(col = coords$colour, fill = coords$colour, lwd = linewidth),  # Use mapped fill
         arrow = arrow
       )
       points_grob <- NULL
