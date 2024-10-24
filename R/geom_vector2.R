1f59f82ed708302a284272b906e212aff51e57be
Turner <dusty.s.turner@gmail.com>
2024-10-24 15:57:39 -0500

    docs and proper aes for vector and vector2

--- R/geom_vector2.R
+++ R/geom_vector2.R
@@ -1,44 +1,45 @@
-#' Create a Vector Plot Layer with Norm Mapped to Color
+#' Create a Vector Plot Layer with Norm Mapped to Length
 #'
 #' `geom_vector2` and `stat_vector2` are extensions of `geom_vector` and `stat_vector`.
-#' They provide identical functionality but map the **vector norm (magnitude)** to
-#' the `color` aesthetic by default. This is useful when you want vector magnitude to
-#' be emphasized visually through color.
+#' These functions provide identical functionality but change the default behavior
+#' to map the **vector norm (magnitude)** to the `length` aesthetic instead of color.
 #'
-#' These functions are wrappers around `geom_vector()` and `stat_vector()`, ensuring
-#' that `color = after_stat(norm)` and `length = after_stat(NA)` are applied by default.
+#' This is useful when you want the vector's magnitude to be visually emphasized by
+#' its length rather than its color. These functions also disable the default
+#' color mapping, setting all vectors to black unless otherwise specified.
+#'
+#' These functions ensure that `length = after_stat(norm)` is applied by default,
+#' while the `color` aesthetic is set to `NULL`. This gives users the ability to
+#' scale vector length by magnitude without visual interference from color mapping.
 #'
 #' @inheritParams geom_vector
 #' @param mapping Aesthetic mappings created by `aes()` or `aes_()`.
-#'   These functions ensure that `color = after_stat(norm)` is mapped by default.
+#'   These functions ensure that `length = after_stat(norm)` is mapped by default,
+#'   and `color` is set to `NULL` unless otherwise specified.
 #' @param ... Other arguments passed on to `geom_vector()` or `stat_vector()`.
 #' @return A ggplot2 layer that can be added to a ggplot object.
-#' @examples
 #'
-#' # Example with Cartesian Data
+#' @examples
 #' set.seed(1234)
 #' n <- 10
 #' wind_data <- data.frame(
 #'   lon = rnorm(n),
 #'   lat = rnorm(n),
 #'   wind_dir = runif(n, -pi, pi),
-#'   wind_spd = rchisq(n, df = 2)
+#'   wind_spd = rchisq(n, df = 2),
+#'   dx = rchisq(n, df = 2) * cos(runif(n, -pi, pi)),
+#'   dy = rchisq(n, df = 2) * sin(runif(n, -pi, pi))
 #' )
-#' wind_data$dx <- wind_data$wind_spd * cos(wind_data$wind_dir)
-#' wind_data$dy <- wind_data$wind_spd * sin(wind_data$wind_dir)
 #'
 #' ggplot(wind_data) +
-#'   geom_vector2(aes(x = lon, y = lat, dx = dx, dy = dy)) +
-#'   scale_color_viridis_c() +
-#'   theme_minimal() +
-#'   labs(color = "Vector Magnitude")
+#'   geom_vector2(aes(x = lon, y = lat, dx = dx, dy = dy))
 #'
 #' # Example with Polar Coordinates
 #' ggplot(wind_data) +
-#'   geom_vector2(aes(x = lon, y = lat, angle = wind_dir, distance = wind_spd)) +
-#'   scale_color_viridis_c() +
-#'   theme_minimal() +
-#'   labs(color = "Vector Magnitude")
+#'   geom_vector2(aes(x = lon, y = lat, angle = wind_dir, distance = wind_spd))
+#'
+#' @seealso
+#' Use [geom_vector()] if you prefer to map vector magnitude to color rather than length.
 #'
 #' @export
 geom_vector2 <- function(
@@ -51,7 +52,7 @@ geom_vector2 <- function(
     show.legend = NA,
     inherit.aes = TRUE
 ) {
-  mapping <- modifyList(aes(color = after_stat(norm), length = after_stat(NA)), mapping)
+  mapping <- modifyList(aes(length = after_stat(norm), color = NULL), mapping)
   geom_vector(
     mapping = mapping,
     data = data,
@@ -76,7 +77,7 @@ stat_vector2 <- function(
     show.legend = NA,
     inherit.aes = TRUE
 ) {
-  mapping <- modifyList(aes(color = after_stat(norm), length = after_stat(NA)), mapping)
+  mapping <- modifyList(aes(length = after_stat(norm), color = NULL), mapping)
   stat_vector(
     mapping = mapping,
     data = data,
